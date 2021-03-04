e65d7592edaa826d784273285af12b1730d17cda
Edson Tadeu Bez <edsonbez@gmail.com>
2021-03-04 12:16:24 -0300

    criei a classe page e removi arquivos de configuração

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use  \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql=new Hcode\DB\Sql();
-	$results=$sql-> select("SELECT * FROM tb_users");
-	echo json_encode($results);
+    $page=new Page(); // nesse momneto chama o construct e adiciona o header na tela
+
+    $page->setTpl("index"); //carega o conteudo e o destruct é automatico e então carrega o footer
 
 });
 
-$app->run();
+$app->run(); // tudo carregado, agora roda
 
  ?>
