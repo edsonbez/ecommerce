35838fe4f800e4f0df8bad9f7720b0d3b616d6b2
Edson Tadeu Bez <edsonbez@gmail.com>
2021-03-08 14:37:44 -0300

    criamos pageadmin e tpl Admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use  \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -16,6 +17,16 @@ $app->get('/', function() {
     $page->setTpl("index"); //carega o conteudo e o destruct é automatico e então carrega o footer
 
 });
+
+$app->get('/admin', function() {
+    
+    $page=new PageAdmin(); // nesse momneto chama o construct e adiciona o header na tela
+
+    $page->setTpl("index"); //carega o conteudo e o destruct é automatico e então carrega o footer
+
+});
+
+
 
 $app->run(); // tudo carregado, agora roda
 
