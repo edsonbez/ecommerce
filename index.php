5c43180b1ce89996acfd4473f8ae9030dd67b616
Edson Tadeu Bez <edsonbez@gmail.com>
2021-04-09 14:56:52 -0300

    site produtos

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once("vendor/autoload.php");
 
 use  \Slim\Slim;
 
-
 $app = new Slim();
 
 $app->config('debug', true);
@@ -16,6 +15,7 @@ require_once("admin.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
 require_once("admin-products.php");
+require_once("functions.php");
 
 $app->run();
 
--- site.php
+++ site.php
@@ -1,13 +1,24 @@
 <?php 
 
-use \Hcode\PageAdmin;
+use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
     
+    $products = Product::listAll();
+
+
     $page=new Page(); // nesse momneto chama o construct e adiciona o header na tela
 
-    $page->setTpl("index"); //carega o conteudo e o destruct é automatico e então carrega o footer
+
+    $page->setTpl("index",[
+    "products"=>Product::checkList($products)
+    ]); //carega o conteudo e o destruct é automatico e então carrega o footer
+
+
+
 
 });
+
 
  ?>
